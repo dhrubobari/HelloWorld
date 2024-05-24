bb942ee0547d71463653304c2686123d480d5535
Dhrubo Bari <baridhrubo@gmail.com>
2024-05-25 02:09:48 +0600

    python day 1: execution, variables, input receive

---
+++ app.py
@@ -0,0 +1,28 @@
+# print("The code will run!")
+# print("Another code will run!")
+# print("*" * 10)
+
+# variables
+
+price = 10
+rating = 4.9
+# print(price)
+
+# name = 'Mosh'
+# is_published = False
+
+# exercise -1
+name = 'John Smith'
+age = 20
+is_new = True
+
+# input will give a value
+# name = input('What is your name?')
+# we will combine 2 strings
+# print('Hi' + name)
+
+
+# exercise -2
+name = input('What is your name?')
+favorite_color = input('What is your favorite color?')
+print(name + ' likes' + favorite_color)
