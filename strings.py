9e7f0ddd2ac83c9ee76f92a9021e1490c1da88c0
Dhrubo Bari <baridhrubo@gmail.com>
2024-05-29 03:31:09 +0600

    python day 5

--- strings.py
+++ strings.py
@@ -1,13 +1,31 @@
-course = 'Python for Beginnners'
-another = course[3:]
+import math
+
+print(math.floor(2.9756))
+
+# course = 'Python for Beginnners'
+# another = course[3:]
 
 # print(another)
-name = 'Jennifer'
+# name = 'Jennifer'
 # print(name[1:-1])
 
 # FORMATTED STRINGS
-first = 'John'
-last = 'Smith'
-message = first + ' [' + last + '] is a coder '
-msg = f'{first} [{last}] is a coder'  # f''(quote) is formatter string
-print(msg);
+# first = 'John'
+# last = 'Smith'
+# message = first + ' [' + last + '] is a coder '
+# msg = f'{first} [{last}] is a coder'  # f''(quote) is formatter string
+# print(msg);
+
+# string methods
+# course = 'Python for Beginnner'
+# print(course.upper())
+
+# x = 10 + 3 * 2 ** 2
+# print(x);
+
+# x = (2 + 3) * 10 - 3
+# print(x)
+
+# x = 2.9
+# print(round(x))
+# print(abs(-2.9))  #absolute returns positive even it's negative
